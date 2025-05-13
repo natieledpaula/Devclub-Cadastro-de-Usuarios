238b7c585d7fdaef8392c36ede3654876ac7eda6
natieledpaula <natiele887@gmail.com>
2025-05-13 15:03:59 -0300

    Começando react em index.jsx

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -5,8 +5,15 @@ function Home() {
         <div className='container'>
             <form>
                 <h1>Cadastro de Usuários</h1>
-                <input type="text" />
+                <input name='nome' type='text'/>
+                <input name='idade' type='number'/>
+                <input name='email' type='email'/>
+                <button type='button'>Cadastrar</button>
             </form>
+
+            <div>
+                
+            </div>
         </div>
     )
 }
